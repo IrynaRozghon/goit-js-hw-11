90df5519bae15880d1a005df24a81bb303d6e1bc
IrynaRozghon <irina.rozgon49@gmail.com>
2023-02-07 12:17:24 +0200

    update async

--- src/index.js
+++ src/index.js
@@ -7,51 +7,77 @@ import {fetchPhoto} from "./javaScript/fetchPhoto";
 import { murcupCards } from "./javaScript/marcupCards";
 import { moreLoadButton } from "./javaScript/moreLoadButton";
 
-const galleryEl = document.querySelector('.gallery');
-const formEl = document.getElementById('search-form');
+const galleryEl = document.querySelector('.gallery')
+const formEl = document.getElementById('search-form')
 let gallery = new SimpleLightbox('.gallery a');
-const btnLoadMore = new moreLoadButton({
-    selector: '.load-more',
-    isHidden: true
-});
+const btnLoadMore = new moreLoadButton({ selector: '.load-more',
+isHidden: true})
+const newApi = new fetchPhoto()
+
 
 formEl.addEventListener('submit', onFormSubmit);
 btnLoadMore.button.addEventListener('click', onBtnLoadMore)
 
-function onFormSubmit(e){
-e.preventDefault();
-const form = e.currentTarget;
-const query = e.target.searchQuery.value.trim();
-clearGallery();
-if(query === '') return
-
-fetchPhoto(query).then(({hits, totalHits}) =>{     
-    hits.length === 0 ? Notify.failure('Sorry, there are no images matching your search query. Please try again.') :
-        Notify.success(`Hooray! We found ${totalHits} images.`)
-        return hits
-     }).then(murcupCards).then(addMarkup)
-
-
-}
-function onBtnLoadMore(){
-    btnLoadMore.disable()
-console.log('yes')
-
-}
-
-function addMarkup(markup){
-    galleryEl.innerHTML = markup
+async function onFormSubmit(e){
+  e.preventDefault();
+  newApi.resetPage()
+  btnLoadMore.hide()
+  const form = e.currentTarget;
+  const query = e.target.searchQuery.value.trim()  
+  newApi.queryItem = query
+  clearGallery();
+  if(query === '') return
+  try{
+    const data = await newApi.fetchCard();
+    const hits = data.hits;
+    const totalHits = data.totalHits;
+    if(hits.length === 0)   throw new Error('error'); 
+    Notify.success(`Hooray! We found ${totalHits} images.`)
+    addMarkup(hits)
+  } catch{
+    onError()
+  } finally{
+    form.reset()
   }
-
-  function updateMarkup(markup){
-galleryEl.insertAdjacentHTML('beforeend', markup)
-  }
-
-  function clearGallery(){
-    galleryEl.innerHTML = ''
-  }
-function onError(error){
-    console.log(error)
-    
 }
 
+async function onBtnLoadMore(){ 
+  btnLoadMore.disable()
+  try{
+    const data = await newApi.fetchCard()
+    const hits = data.hits
+    addMarkup(hits)
+  } catch(error) {
+    onError()
+  } 
+}
+
+ async function addMarkup(hits){
+  const hitsLength = hits.length
+  if(hitsLength < 40){
+    Notify.info('We are sorry, but you have reached the end of search results.')
+    const markup = await murcupCards(hits)
+    updateMarkup(markup)
+    gallery.refresh();
+    btnLoadMore.hide()
+    return
+  }
+  const markup = await murcupCards(hits)
+  updateMarkup(markup)
+  gallery.refresh();
+  btnLoadMore.show()
+  btnLoadMore.enable()
+}
+
+
+function updateMarkup(markup){
+  galleryEl.insertAdjacentHTML('beforeend', markup)
+}
+
+function clearGallery(){
+  galleryEl.innerHTML = ''
+}
+
+function onError(){
+  Notify.failure('Sorry, there are no images matching your search query. Please try again.')    
+}
--- src/javaScript/fetchPhoto.js
+++ src/javaScript/fetchPhoto.js
@@ -1,7 +1,21 @@
 import axios from 'axios';
 const ENDPOINT = "https://pixabay.com/api/";
 const API_KEY = '33288510-5b2f908f6ba0ee4c3753fbb26';
-function fetchPhoto(query, numPage = 1){
-    return axios.get(`${ENDPOINT}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${numPage}&per_page=40`).then(response => response.data)
+class fetchPhoto{
+    constructor(){
+        this.queryItem = '';
+        this.pageNum = 1;
+    }
+     async fetchCard(){
+        const response = await axios.get(`${ENDPOINT}?key=${API_KEY}&q=${this.queryItem}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.pageNum}&per_page=40`)
+        this.incrementPage();           
+        return response.data
+    }
+    resetPage(){
+        this.pageNum = 1
+    }
+    incrementPage(){
+        this.pageNum += 1
+    }
 }
 export {fetchPhoto};
